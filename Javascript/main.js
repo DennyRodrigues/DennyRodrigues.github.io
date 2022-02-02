c8617793ccdb908f72fea175596ac46dc576c806
DennyRodrigues <denny.rodrigues.carmo@gmail.com>
2022-02-02 16:28:56 -0300

    commit on main

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,54 +1,81 @@
-var seconds = document.querySelector("#seconds");
-var minutes = document.querySelector("#minutes");
-var set_timer = document.querySelector('#set_timer');
-var stop_timer = document.querySelector('#stop_timer');
-
+let seconds = document.querySelector("#seconds");
+let minutes = document.querySelector("#minutes");
+let startTimer = document.querySelector("#start_timer");
+let resetTimer = document.querySelector("#reset_timer");
+let timerIsRunning = false;
+let interval;
 // The counter will increase 1 each second.
-var counter = 0;
+let counter = 0;
 // will be used to get the input from the user
-var start_time = 0;
-// Total_time is the total seconds value without converting to minutes
-var total_time = 0;
-// 
-var call_clock;
+let startInput = 0;
+// Total_timer is the total seconds value without converting to minutes
+let total_timer = 0;
+//
+let call_clock;
 
-
-//change values of seconds and minutes using counter and total_time
-function convert_values(){
-    seconds.value = (total_time - counter) % 60;
-    minutes.value = Math.floor((total_time - counter)/60);
-    
+//change values of seconds and minutes using counter and total_timer
+function convert_values() {
+  seconds.value = (total_timer - counter) % 60;
+  minutes.value = Math.floor((total_timer - counter) / 60);
 }
 //Increase counter, stop with value reach 0, and call convert_values.
-function clock(){
-    counter++;
-    convert_values();
-    if ((total_time - counter) == 0){
-        clearInterval(interval);
-    }
-    if (seconds < 0){
-        clearInterval(interval);
-    }
-    
+function clock() {
+  counter++;
+  convert_values();
+  if (total_timer - counter == 0) {
+    clearInterval(interval);
+    stopCouting();
+  }
+  if (seconds < 0) {
+    clearInterval(interval);
+    stopCouting();
+  }
 }
 
 //set timer button, add new values to the total timer and start the count.
-set_timer.addEventListener("click", function start_timer() {
-    //Reset counter
+function startHandler() {
+  if (timerIsRunning) {
+    stopCouting();
+  } else {
+    // Reset counter
     counter = 0;
-    //check if the start_time value is positive and then add it to the total_time.
-    start_time = Number(minutes.value * 60) + Number(seconds.value);
-    if (start_time > 0){
-        total_time = start_time;
-        //Start the clock
-        interval = setInterval(clock,1000);
-    }
-    
-    
+    // check if the startInput value is positive and then add it to the total_timer.
+    startInput = Number(minutes.value * 60) + Number(seconds.value);
+    if (startInput > 0) {
+      total_timer = startInput;
+      //Start the clock
+      interval = setInterval(clock, 1000);
 
-});
-//Stop timer Button, stop the clock
-stop_timer.addEventListener("click", function stop(){
-    clearInterval(interval);
-    
-});
+      // CHANGE THE BUTTON FROM SET TO STOP timer
+      startTimer.classList.add("stop_timer");
+      startTimer.classList.remove("start_timer");
+      startTimer.innerHTML = "Stop";
+      timerIsRunning = true;
+    } else {
+      // The input(minutes and seconds) will reset if the user tries to enter a negative value
+      resetTimerHandler();
+    }
+  }
+}
+
+function stopCouting() {
+  clearInterval(interval);
+  timerIsRunning = false;
+  // CHANGE THE BUTTON FROM STOP TO SET timer
+  startTimer.classList.add("start_timer");
+  startTimer.classList.remove("stop_timer");
+  startTimer.innerHTML = "Start";
+}
+
+//start timer Button, stop the clock
+startTimer.addEventListener("click", startHandler);
+
+// RESET TIMER BUTTON
+function resetTimerHandler() {
+  stopCouting();
+  counter = 0;
+  seconds.value = 0;
+  minutes.value = 0;
+}
+
+resetTimer.addEventListener("click", resetTimerHandler);
